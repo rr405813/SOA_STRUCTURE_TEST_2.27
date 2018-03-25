ed2ac926900f7a6cf514682c0ad2fcb7ddb5c259
rr405813 <rr405813@ohio.edu>
2018-03-25 18:55:05 -0400

    03.25 mouseover audio
    Audio added to menu structure

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+/*  splash page fading in and out code taken from https://stackoverflow.com/questions/20823767/welcome-screen-before-website-loads-click-to-enter-splash-screen*/
+ $('.enter_link').click(function() {
+        $(this).parent().fadeOut(1000);
+ });
+
+/* hover over menu sections audio turn on/off 
+code taken from http://jsfiddle.net/PsyWolf/W3XdA/8/ */
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+/* expand/shrink menu sections homepage */
 $(function() {
     $('.home_nav_section').hover(function() {
         $(this).toggleClass('expand');
@@ -18,7 +37,7 @@ $(function() {
 
 jQuery(function($) {
     setSkrollr($('.highlighter'), [[0, 'transform:translateX(-100%)'], [750, 'transform:translateX(100%)'], [1500, 'transform:translateX(-100%)']]);
-    
+
     skrollr.init({
         smoothScrolling: false
     });
